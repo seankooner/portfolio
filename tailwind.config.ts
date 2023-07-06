b9ea5dcd45155c4ea8fee8dadb2d1f0a46e78086
Sean Kooner <sean@buildasoil.com>
2023-07-06 13:11:00 -0400

    Packages and configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,17 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        "2xs": ["0.65rem", "0.7rem"],
+      },
+      backgroundImage: () => ({
+        "overlay-gradient":
+          "linear-gradient(135deg, rgba(106, 0, 255, 0.5) 0%, rgba(0, 255, 222, 0.5) 100%)",
+        "container-gradient-dark":
+          "linear-gradient(135deg, rgba(54, 12, 81, 0.5) 0%, rgba(9, 9, 121, 0.5) 100%)",
+      }),
+    },
   },
   plugins: [],
 } satisfies Config;
